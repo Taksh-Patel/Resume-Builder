2a4b39f425b5e7e784c4619b1f63ff975594faaa
Taksh Patel <tkpatel8055@gmail.com>
2022-05-12 16:54:24 +0530

    updated testcases

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,11 @@ import { shallow,mount } from 'enzyme';
 import LandingPage from './components/presentation/landingPage';
 import Education from './components/presentation/education';
 import GettingStarted from './components/presentation/gettingStarted';
+import AboutUs from './components/presentation/aboutUs';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'; 
+import rootReducer from './redux/reducers/rootReducer';
+
 
 it("renders without crashing", () => {
   shallow(<App />);
@@ -11,39 +16,24 @@ it("renders without crashing", () => {
 
 it("renders Landing page 1", () => {
   const wrapper = shallow(<LandingPage />);
-  const welcome = <h1>Create a resume that stands out</h1>;
-  expect(wrapper.contains(welcome)).toEqual(true);
+  const w = <h1>Create a resume that stands out</h1>;
+  expect(wrapper.contains(w)).toEqual(true);
 });
 
 it("renders Landing page 2", () => {
   const wrapper = shallow(<LandingPage />);
-  const welcome = <span>Get Started for Free</span>;
-  expect(wrapper.contains(welcome)).toEqual(true);
+  const w = <span>Get Started for Free</span>;
+  expect(wrapper.contains(w)).toEqual(true);
 });
-//
-/*it("renders Landing page", () => {
+
+it("renders Landing page 3", () => {
   const wrapper = shallow(<LandingPage />);
-  const welcome = <h1>Select a resume template to get started</h1>;
-  expect(wrapper.contains(welcome)).toEqual(true);
+  const w = <p >Create a Resume that perfectally describes your skils and match job profile.</p>;
+  expect(wrapper.contains(w)).toEqual(true);
 });
 
-const eprops = {
-  SchoolName: "IIITB",
-  Degree: "MTech",
-  GraduationCGPA: "3.6",
-  City: "Banglore",
-  GraduationDate: "6-may",
-  GraduationYear: "2023"
-};
-
-describe("", () => {
-  it("accepts user education props", () => {
-    const wrapper = mount(<Education props={eprops} />);
-    expect(wrapper.props().props).toEqual(eprops);
-  });
-  //it("contains users account email", () => {
-    //const wrapper = mount(<Education props={eprops} />);
-    //const value = wrapper.find("p").text();
-    //expect(value).toEqual("david@gmail.com");
-  //});
-});*/
+it("renders AboutUs page", () => {
+  const wrapper = shallow(<AboutUs/>);
+  const w = <p>Do you have any comments or questions? Our team will be happy to assist you. Send us a message.</p>;
+  expect(wrapper.contains(w)).toEqual(true);
+});
